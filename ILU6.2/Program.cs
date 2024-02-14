a1dea58afd17f5f15cc60bac1de3c502a419844b
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-14 10:46:37 +0100

    Add project files.

---
+++ ILU6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ILU6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Störst(23,12));
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            int störst;
+            if (tal1 < tal2) 
+            {
+                störst = tal2;
+            }
+            else if(tal2 < tal1) 
+            { 
+                störst=tal1;
+            }
+            else
+            {
+                störst=tal1;
+            }
+            return störst;
+        }
+
+    }
+}
